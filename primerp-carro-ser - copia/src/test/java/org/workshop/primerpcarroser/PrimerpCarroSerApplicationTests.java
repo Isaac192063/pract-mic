2c0e1c3e01f6b1bf42d7b90838d66242f228ce3b
isaac192063 <ichinchillac@ufpso.edu.co>
2024-06-25 22:32:40 -0500

    multiples-instances

---
+++ primerp-carro-ser - copia/src/test/java/org/workshop/primerpcarroser/PrimerpCarroSerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.workshop.primerpcarroser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerpCarroSerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
